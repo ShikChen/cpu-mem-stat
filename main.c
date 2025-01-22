2c6f3e2e42a316d503aedb3587f876ac00e28249
Shik Chen <shik.chen@gmail.com>
2025-01-23 04:28:33 +0800

    Refactor with a mem_stat struct

--- main.c
+++ main.c
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct {
+  int64_t used;
+  int64_t total;
+} mem_stat;
+
 const long long MEM_FORMAT_THRESHOLD = 8LL << 30;
 
 #if defined(__APPLE__) && defined(__MACH__)
@@ -22,22 +27,23 @@ void get_cpu_stat(long long *tot, long long *idle) {
   for (int i = 0; i < CPU_STATE_MAX; i++) *tot += stat.cpu_ticks[i];
 }
 
-void get_mem_usage(long long *mem_used, long long *mem_total) {
-  int ret;
+mem_stat get_mem_usage(void) {
   vm_size_t page_size;
-  ret = host_page_size(mach_host_self(), &page_size);
+  int ret = host_page_size(mach_host_self(), &page_size);
   assert(ret == KERN_SUCCESS);
   vm_statistics64_data_t stat;
   mach_msg_type_number_t count = HOST_VM_INFO64_COUNT;
   ret = host_statistics64(mach_host_self(), HOST_VM_INFO64,
                           (host_info64_t)&stat, &count);
   assert(ret == KERN_SUCCESS);
-  *mem_used = (stat.internal_page_count - stat.purgeable_count +
-               stat.wire_count + stat.compressor_page_count) *
-              page_size;
-  size_t tot_size = sizeof(*mem_total);
-  ret = sysctlbyname("hw.memsize", mem_total, &tot_size, NULL, 0);
+  int64_t used = (int64_t)(stat.internal_page_count - stat.purgeable_count +
+                           stat.wire_count + stat.compressor_page_count) *
+                 page_size;
+  int64_t total = 0;
+  size_t tot_size = sizeof(total);
+  ret = sysctlbyname("hw.memsize", &total, &tot_size, NULL, 0);
   assert(ret == 0);
+  return (mem_stat){.used = used, .total = total};
 }
 
 #else
@@ -51,21 +57,17 @@ void get_cpu_stat(long long *tot, long long *idle) {
   *tot = user + nice + system + *idle;
 }
 
-void get_mem_usage(long long *mem_used, long long *mem_total) {
+mem_stat get_mem_usage(void) {
   FILE *fp = fopen("/proc/meminfo", "r");
   assert(fp != NULL);
-  long long mem_free, mem_available;
-  int ret =
-      fscanf(fp, "MemTotal: %lld kB\nMemFree: %lld kB\n", mem_total, &mem_free);
+  long long mem_total = 0, mem_unused = 0;
+  int ret = fscanf(fp, "MemTotal: %lld kB\nMemFree: %lld kB\n", &mem_total,
+                   &mem_unused);
   assert(ret == 2);
-  *mem_total <<= 10;
-  mem_free <<= 10;
-  if (fscanf(fp, "MemAvailable: %lld kB\n", &mem_available) == 1) {
-    mem_available <<= 10;
-    *mem_used = *mem_total - mem_available;
-  } else {
-    *mem_used = *mem_total - mem_free;
-  }
+  // May not have MemAvailable on older kernels.
+  fscanf(fp, "MemAvailable: %lld kB\n", &mem_unused);
+  return (mem_stat){.used = (mem_total - mem_unused) << 10,
+                    .total = mem_total << 10};
 }
 
 #endif
@@ -92,14 +94,13 @@ int main(int argc, char *argv[]) {
     sample_duration = 100000;
   }
   double cpu_usage = get_cpu_usage(sample_duration);
-  long long mem_used, mem_total;
-  get_mem_usage(&mem_used, &mem_total);
-  if (mem_total <= MEM_FORMAT_THRESHOLD) {
-    printf("%4.0f%% %lld/%lldM\n", cpu_usage * 100, mem_used >> 20,
-           mem_total >> 20);
+  mem_stat mem = get_mem_usage();
+  if (mem.total <= MEM_FORMAT_THRESHOLD) {
+    printf("%4.0f%% %lld/%lldM\n", cpu_usage * 100, mem.used >> 20,
+           mem.total >> 20);
   } else {
-    printf("%4.0f%% %.1f/%.1fG\n", cpu_usage * 100, 1.0 * mem_used / (1 << 30),
-           1.0 * mem_total / (1 << 30));
+    printf("%4.0f%% %.1f/%.1fG\n", cpu_usage * 100, 1.0 * mem.used / (1 << 30),
+           1.0 * mem.total / (1 << 30));
   }
   return 0;
 }
