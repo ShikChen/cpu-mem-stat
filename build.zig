528c6e918530df77eb65bde1070639cf1204b2a9
Shik Chen <shik.chen@gmail.com>
2025-01-24 18:12:27 +0800

    Add `zig build release` to build all release targets

--- build.zig
+++ build.zig
@@ -1,6 +1,36 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+};
+
+pub fn buildRelease(b: *std.Build) !void {
+    const release_step = b.step("release", "Build release targets");
+    for (targets) |target| {
+        const target_exe = b.addExecutable(.{
+            .name = "cpu-mem-stat",
+            .target = b.resolveTargetQuery(target),
+            .optimize = .ReleaseSmall,
+        });
+        target_exe.addCSourceFiles(.{ .files = &.{"main.c"} });
+        target_exe.linkLibC();
+        const target_output = b.addInstallArtifact(target_exe, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = try target.zigTriple(b.allocator),
+                },
+            },
+        });
+        release_step.dependOn(&target_output.step);
+    }
+}
+
+pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{
         .name = "cpu-mem-stat",
         .target = b.standardTargetOptions(.{}),
@@ -13,4 +43,6 @@ pub fn build(b: *std.Build) void {
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
+
+    try buildRelease(b);
 }
