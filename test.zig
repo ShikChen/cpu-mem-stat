6ddfd51071b07fd70de5acf08009b95155859c0c
Shik Chen <shik.chen@gmail.com>
2025-01-27 16:44:04 +0800

    Add more tests for format_stats

--- test.zig
+++ test.zig
@@ -11,6 +11,8 @@ test "get_cpu_stat" {
     const stat = c.get_cpu_stat();
     try expect(stat.idle >= 0);
     try expect(stat.total > 0);
+    // We cannot check idle <= total because the ticks might have wrapped
+    // around.
 }
 
 test "get_mem_stat" {
@@ -25,10 +27,34 @@ test "sample_cpu_usage" {
     try expect(usage >= 0 and usage <= 100);
 }
 
-test "format_stats" {
+fn test_format(
+    cpu: c_int,
+    mem_used: i64,
+    mem_total: i64,
+    expected: []const u8,
+) !void {
     var buf = [_:0]u8{0} ** 128;
-    const mem = c.mem_stat{ .used = 1 << 30, .total = 2 << 30 };
-    c.format_stats(50, mem, &buf[0], buf.len);
+    const mem = c.mem_stat{ .used = mem_used, .total = mem_total };
+    c.format_stats(cpu, mem, &buf[0], buf.len);
     const str = std.mem.span(@as([*:0]u8, buf[0..]));
-    try expectEqualStrings(" 50% 1024/2048M", str);
+    try expectEqualStrings(expected, str);
+}
+
+test "format_stats" {
+    const M: i64 = 1 << 20;
+    const G: i64 = 1 << 30;
+    const T: i64 = 1 << 40;
+
+    try test_format(0, 1 * G, 2 * G, "  0% 1024/2048M");
+    try test_format(50, 1 * G, 2 * G, " 50% 1024/2048M");
+    try test_format(100, 1 * G, 2 * G, "100% 1024/2048M");
+
+    try test_format(50, 5566 * M, 8 * G, " 50% 5566/8192M");
+    try test_format(50, 5566 * M, 8193 * M, " 50% 5.4/8G");
+    try test_format(50, 8 * G, 315 * G / 10, " 50% 8.0/31.5G");
+    try test_format(50, 8 * G, 32 * G, " 50% 8.0/32G");
+    try test_format(50, 50 * G, 9994 * G / 100, " 50% 50.0/99.9G");
+    try test_format(50, 50 * G, 9996 * G / 100, " 50% 50/100G");
+
+    try test_format(50, 1 * T, 256 * T, " 50% 1/256T");
 }
